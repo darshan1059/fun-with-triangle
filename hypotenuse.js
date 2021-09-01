11229627b5950b377427db34fdbfdbfe64166a05
darshan1059 <chandradarshanagrawal1059@gmail.com>
2021-09-01 15:55:22 +0530

    created hypotenuse.html, hypotenuse.js

---
+++ hypotenuse.js
@@ -0,0 +1,17 @@
+const inputSides = document.querySelectorAll(".side-input");
+const hypotenuseButton = document.querySelector("#hypotenuse-button");
+const outputDiv = document.querySelector("#output");
+
+hypotenuseButton.addEventListener("click", calculateHypotenuse);
+
+function calculateHypotenuse(){
+    const sumOfSquares = calculateSumOfSquares(Number(inputSides[0].value), Number(inputSides[1].value));
+    const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+    outputDiv.innerText = "The length of hypotenuse is " + lengthOfHypotenuse;
+
+}
+
+function calculateSumOfSquares(a, b){
+    const sumOfSquares = a*a + b*b;
+    return sumOfSquares;
+} 
